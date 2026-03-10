8c85d283f64ed7a47033a2d12c38d11403b6077e
xuan <jimwong922@gmail.com>
2026-03-11 01:20:28 +0800

    Add files via upload

---
+++ pro/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'shixu-pro-v1'; // 提升版本号
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll([
+    './', 
+    './index.html', 
+    './manifest.json', 
+    './shixu.png', // 确保包含这个图片文件
+    'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js'
+  ])));
+});
